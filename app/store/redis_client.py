736044b0039714b8cad58d7ea01bbe9a4325bf84
Pawan Matade <admin@dematadesolution.com>
2026-01-02 09:48:01 +0530

    change664dec2025

--- app/store/redis_client.py
+++ app/store/redis_client.py
@@ -1,12 +1,28 @@
 import redis
 import os
+from redis.exceptions import RedisError
 
-def get_redis_client():
-    return redis.Redis(
-        host=os.environ["REDIS_HOST"],
-        port=int(os.environ.get("REDIS_PORT", 6379)),
-        db=int(os.environ.get("REDIS_DB", 0)),
-        decode_responses=True,
-    )
+redis_kwargs = {
+    'host': os.environ["REDIS_HOST"],
+    'port': int(os.environ.get("REDIS_PORT", 6379)),
+    'db': int(os.environ.get("REDIS_DB", 0)),
+    'decode_responses': True
+}
+if os.environ.get("REDIS_PASSWORD"):
+    redis_kwargs['password'] = os.environ.get("REDIS_PASSWORD")
 
-redis_client = get_redis_client()
+redis_client = redis.Redis(**redis_kwargs)
+
+
+def test_connection():
+    """
+    Test Redis connection by pinging the server.
+    
+    Returns:
+        bool: True if Redis is reachable, False if any exception occurs
+    """
+    try:
+        redis_client.ping()
+        return True
+    except RedisError:
+        return False
